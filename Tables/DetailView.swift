698f9fa103404b5ba0abd1e2a67a2b7d9d39e8de
Manuel E. Hernandez <kicke10h@gmail.com>
2022-07-03 22:19:42 -0500

    Make Dynamic Interface for DetailView

--- Tables/DetailView.swift
+++ Tables/DetailView.swift
@@ -8,17 +8,18 @@
 import SwiftUI
 
 struct DetailView: View {
+    let character: Character
     var body: some View {
         NavigationView {
             VStack {
-                Image("Morty")
+                Image(character.img)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(maxWidth: 100)
                     .clipShape(Circle())
-                Text("Morty Smith").font(.title)
-                Text("ID: 1").font(.title2)
-                Text("Age: 16").font(.title3)
+                Text("\(character.name)").font(.title)
+                Text("ID: \(character.id)").font(.title2)
+                Text("Age: \(character.age)").font(.title3)
             }
         }
     }
@@ -26,6 +27,6 @@ struct DetailView: View {
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView()
+        DetailView(character: Character.sampleCharacter)
     }
 }
