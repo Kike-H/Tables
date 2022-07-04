8250300d1e076260bbabe0308ecd57f941175ad6
Manuel E. Hernandez <kicke10h@gmail.com>
2022-07-03 21:02:12 -0500

    Initial Commit

---
+++ Tables/TablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TablesApp.swift
+//  Tables
+//
+//  Created by Kike Hernandez  on 03/07/22.
+//
+
+import SwiftUI
+
+@main
+struct TablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
